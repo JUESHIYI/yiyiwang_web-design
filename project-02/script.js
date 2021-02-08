b940115c8f7982690ee8d2dc5db7d2f6ab967f46
Yiyi Wang <77234818+JUESHIYI@users.noreply.github.com>
2021-02-08 17:30:44 +0800

    U

--- project-02/script.js
+++ project-02/script.js
@@ -1,112 +1,49 @@
+$("document").ready(function () {  
 
-$("document").ready(function() {
+  $(".menu").click(function () {
+    $("nav").toggle();
+  });
 
-    filterSelection("all")
-    function filterSelection(c) {
-        var x, i;
-  x = document.getElementsByClassName("work");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }}
-
-  // Show filtered elements
-  function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
-      }
-    }}
-  
-    // Hide elements that are not selected
-    function w3RemoveClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-          while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-          }
-        }
-        element.className = arr1.join(" ");}
-
-    var btnContainer = document.getElementById("myBtnContainer");
-    var btns = btnContainer.getElementsByClassName("btn");
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });}
+  $(".ins").mouseover(function () {
+    $(".qr-codes").show();
+    $(".ins-code").show();
 
 
-  
+  }).mouseleave(function () {
+    $(".qr-codes").hide();
+    $(".ins-code").hide();
+
+  });
+
+  $(".snap").mouseover(function () {
+    $(".qr-codes").show();
+    $(".snap-code").show();
 
 
+  }).mouseleave(function () {
+    $(".qr-codes").hide();
+    $(".snap-code").hide();
 
-    $(".menu").click(function() {
-        $("nav").toggle();
-    });
-
-    $(".ins").mouseover(function(){
-        $(".qr-codes").show();
-        $(".ins-code").show();
-
-     
-    }).mouseleave(function(){
-        $(".qr-codes").hide();
-        $(".ins-code").hide();
-
-    });
-
-    $(".snap").mouseover(function(){
-        $(".qr-codes").show();
-        $(".snap-code").show();
-
-     
-    }).mouseleave(function(){
-        $(".qr-codes").hide();
-        $(".snap-code").hide();
-
-    });
- 
-
-    $(".wechat").mouseover(function(){
-        $(".qr-codes").show();
-        $(".wechat-code").show();
-
-     
-    }).mouseleave(function(){
-        $(".qr-codes").hide();
-        $(".wechat-code").hide();
-
-    });
+  });
 
 
+  $(".wechat").mouseover(function () {
+    $(".qr-codes").show();
+    $(".wechat-code").show();
 
 
-   
-    $("#latestwork-text01").arctext({radius: 538,dir: -1});
-    $("#latestwork-text02").arctext({radius: 538});
-    
-    
-    
-    
-    
+  }).mouseleave(function () {
+    $(".qr-codes").hide();
+    $(".wechat-code").hide();
+
+  });
+
+  $("#latestwork-text01").arctext({ radius: 538, dir: -1 });
+  $("#latestwork-text02").arctext({ radius: 538 });
 
 
-    
-    
-
-    
 
 
 });
-
 
 
